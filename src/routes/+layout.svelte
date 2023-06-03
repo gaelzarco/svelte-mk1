984a2346d4dd199985c75c374df508858eae2aa1
gael <76548719+gaelzarco@users.noreply.github.com>
2023-06-03 16:22:19 -0700

    Progress learning Svelte

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+	import './styles.css';
+</script>
+
+<main class="app">
+	<slot />
+</main>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+		min-width: 100vw;
+		align-items: center;
+		justify-content: center;
+		justify-items: center;
+		text-align: center;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	let countArr: Array<number> = [];
+	let count = 0;
+
+	function increment() {
+		count += 1;
+		countArr = [...countArr, count]
+	}
+
+	$: doubled = count * 2;
+</script>
+
+<svelte:head>
+	<title>SVELTE</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<button on:click={increment}>
+		Count is: {count}
+	</button>
+	<p>Count doubled is: {doubled}</p>
+
+	<div class='flex-nums'>
+		<p>All values</p>
+		{#each countArr as num}
+			<p>
+				{num}
+			</p>
+		{/each}
+	</div>
+</section>
+
+<style>
+	.flex-nums {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.flex-nums p {
+		margin: 0 10px;
+	}
+</style>
