259c4ebb671fc2665b1c67f67e72d74d6daf0351
gael <76548719+gaelzarco@users.noreply.github.com>
2023-06-07 16:51:13 -0700

    Working with nested objects in Svelte

--- src/routes/arrayofmaps/+page.svelte
+++ src/routes/arrayofmaps/+page.svelte
@@ -1,7 +1,28 @@
 <script lang='ts'>
+    type User = {
+        name: string,
+        age: number
+    }
+    let jeff: User = {
+        name: 'john',
+        age: 12
+    }
+    let jane: User = {
+        name: 'jane',
+        age: 18
+    }
+    let god: User = {
+        name: 'god',
+        age: 40000
+    }
 
+    const array: User[] = [jeff, jane, god]
 </script>
 
 <section>
     <h1>Under construction</h1>
+
+    {#each array as user, idx}
+        <p>{user.name} is at index {idx}</p>
+    {/each}
 </section>
